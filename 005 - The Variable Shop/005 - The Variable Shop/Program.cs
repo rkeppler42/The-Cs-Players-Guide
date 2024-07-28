f458dd726602c5adad30d7683f42b6599c265d44
Roberto Keppler <roberto.keppler@hotmail.com>
2024-07-28 00:05:46 -0300

    Added the "The Variable Shop" exercise.

---
+++ 005 - The Variable Shop/005 - The Variable Shop/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Objectives:
+• Build a program with a variable of all fourteen types described in this level.
+• Assign each of them a value using a literal of the correct type.
+• Use Console.WriteLine to display the contents of each variable
+*/
+
+byte a = 2;
+short b = 32760;
+int c = 32800;
+long d = 3_000_000_000;
+sbyte e = -128;
+ushort f = 60000;
+uint g = 4_000_000_000;
+ulong h = 7_000_000_000;
+float i = 0.32f;
+double j = 0.32;
+decimal k = 0.32m;
+char l = 'r';
+string m = "Hi!";
+bool n = true;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
