f007134488e568a7a3f259e90f01576072bde864
Roberto Keppler <roberto.keppler@hotmail.com>
2024-07-28 00:12:12 -0300

    Added the "The variable Shop Returns" exercise.

---
+++ 006 - The Variable Shop Returns/006 - The Variable Shop Returns/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Objectives:
+• Modify your Variable Shop program to assign a new, different literal value to each of the 14 original
+variables. Do not declare any additional variables.
+• Use Console.WriteLine to display the updated contents of each variable.
+ */
+
+byte a = 2;
+short b = 32760;
+int c = 32800;
+long d = 3_000_000_000;
+sbyte e = -128;
+ushort f = 60000;
+uint g = 4_000_000_000;
+ulong h = 7_000_000_000;
+float i = 0.32f;
+double j = 0.32;
+decimal k = 0.32m;
+char l = 'r';
+string m = "Hi!";
+bool n = true;
+
+// updates:
+
+a = 3;
+b = 2;
+c = 100;
+d = 101;
+e = 42;
+f = 27;
+g = 11;
+h = 6;
+i = 0.001f;
+j = 0.00001;
+k = 0.01m;
+l = 'n';
+m = "Bye!";
+n = false;
+ 
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
